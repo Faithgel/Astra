89ded04e607bbdb493cecfcab786f846c09d493e
Faithgel <angelo.orellana.agui@gmail.com>
2024-03-29 15:18:36 -0300

    Refactor main.cpp to include main.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,5 @@
-// std includes
-#include <cstdlib>
-#include <iostream>
-#include <string>
-// dpp includes
-#include <dpp/dpp.h>
-// logger includes
-#include <spdlog/spdlog.h>
-#include <spdlog/async.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/sinks/rotating_file_sink.h>
-#include <fmt/format.h>
-#include <iomanip>
+// Note: Main entry point for the bot
+#include "main.hpp"
 
 using namespace std;
 int main()
---
+++ src/main.hpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+// dpp includes
+#include <dpp/dpp.h>
+// logger includes
+#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <fmt/format.h>
+#include <iomanip>
+
